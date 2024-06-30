4b4cf4c7a29e430f3240c16bd600ab1873df1594
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2024-06-30 13:07:47 +0530

    Added UI Elements

--- src/routes/(tools)/website-links-count/+page.svelte
+++ src/routes/(tools)/website-links-count/+page.svelte
@@ -1,11 +1,25 @@
-<script>
+<script lang="ts">
+	import { Input, ButtonGroup, Label, Textarea, Helper, Button } from 'flowbite-svelte';
+	export let data;
 
+	var url;
+	var result;
 </script>
 
-<div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
-	<!-- Add tool here -->
+
+<div class="card gap-16 items-center mx-auto max-w-screen-xl overflow-hidden rounded-lg">
+	<div class="p-8">
+
+		<Label for="input-addon" class="mb-2">Enter URL</Label>
+		<ButtonGroup class="w-full">
+			<Input id="input-addon" type="url" placeholder="https://example.com/" bind:value={url}/>
+			<Button color="primary">Search</Button>
+		</ButtonGroup>
+
+		<Label for="textarea-id" class="mt-4 mb-2">Result</Label>
+		<Textarea id="textarea-id" placeholder="List of links will appear here" rows="4" bind:value={result}/>
+
+	</div>
 </div>
 
-<style>
 
-</style>
